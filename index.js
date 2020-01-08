1bc13430f128e0eb1625a2c8ea60756f6e749b64
KatherineMcT <kmctaggart@rbi.com>
2020-01-08 16:25:04 -0500

    printing 12 atricles

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-$(function() {
-  console.log("ready!");
+$("#sectionSelect").on("change", function() {
+  let selectedVal = $("#sectionSelect :selected").val();
+  let sectionURL = `https://api.nytimes.com/svc/topstories/v2/${selectedVal}.json`;
+  console.log(sectionURL);
+
+  $.ajax({
+    method: "GET",
+    url: sectionURL,
+    data: {
+      "api-key": "GppNAXaGs8XSib9sBWqR2wdgv7kFgqKi"
+    }
+  }).done(function(data) {
+    // let disImg = 0;
+    for (i = 0; i < 12; i++) {
+      console.log(data.results[i].abstract);
+    }
+  });
 });
+// console.log(data);
+// console.log(data.results[0].abstract);
+// console.log(data.results[0].url);
+// console.log(data.results[0].multimedia[0].url);

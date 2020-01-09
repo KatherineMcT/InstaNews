af462e85d8ce1ff30a10b71637e32a63be0adbcc
KatherineMcT <kmctaggart@rbi.com>
2020-01-09 13:44:49 -0500

    working loop commit

--- index.js
+++ index.js
@@ -12,10 +12,25 @@ $("#sectionSelect").on("change", function() {
   }).done(function(data) {
     // let disImg = 0;
     for (i = 0; i < 12; i++) {
+      //if(data.results[i].multimedia.length >=1){}
       console.log(data.results[i].abstract);
+
+      $(".container-category").append(`<div>
+      <a href="${data.results[i].url}"><img src=${data.results[i].multimedia[3].url}>
+      </a><p>${data.results[i].abstract}</p>
+      </div>`);
+
+      console.log(data);
     }
   });
 });
+// Show 12 articles in the grid
+// $(".container-category").append<p>${data.results[i].abstract}</p>
+//Redirect to Url
+//Show image in the grid
+// $(".container-category").append(
+//   `<img src=${data.results[i].multimedia[3].url}>`
+// );
 // console.log(data);
 // console.log(data.results[0].abstract);
 // console.log(data.results[0].url);
